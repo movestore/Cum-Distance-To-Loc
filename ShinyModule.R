c57a3bf136296c32427cdba345c0b8da871381fc
annescharf <ascharf@ab.mpg.de>
2022-12-21 22:33:22 +0100

    testing bookmark

--- ShinyModule.R
+++ ShinyModule.R
@@ -3,52 +3,49 @@ library('shiny')
 library('foreach')
 library('fields')
 library('geosphere')
+library("shinycssloaders")
 
 
 Sys.setenv(tz="UTC")
 options(scipen=999)
 #data <- readRDS("input_lowres_geese_74X.rds")
 
+## is this ok? at first it will calculate the distance to coords (0,0).... If it should be a prefixed coorinate, not sure if a shiny app is needed....
+## Go! button can be put in better place (ie alligned with the lat/long boxes)
+## spinner option can be modified to different shapes and colors
 
-shinyModuleUserInterface <- function(id, label, posi_lon=0, posi_lat=0) {
+shinyModuleUserInterface <- function(id, label) {
   ns <- NS(id)
   
   tagList(
     titlePanel("Distance to Location over Time"),
-    numericInput(ns("posi_lon"), "Longitude of Reference Location:", posi_lon, min = -180, max = 180,step=0.00001),
-    verbatimTextOutput("value1"),
+    fluidRow(
+      column(3, numericInput(ns("posi_lon"), "Longitude of Reference Location:", value=0, min = -180, max = 180,step=0.00001)),
+    # verbatimTextOutput("value1"),
     
-    numericInput(ns("posi_lat"), "Latitude of Reference Location:", posi_lat, min = -90, max = 90,step=0.00001),
-    verbatimTextOutput("value2"),
+    column(3, numericInput(ns("posi_lat"), "Latitude of Reference Location:", value=0, min = -90, max = 90,step=0.00001)),
+    # verbatimTextOutput("value2"),
 
-    plotOutput(ns("timeline"),height="90vh")
+    column(1, actionButton(ns("GoButton"), label="Go!"))
+    ),
+  withSpinner(plotOutput(ns("timeline"),height="80vh"))#, type=5, size=1.5,color= "#28b78d") 
   )
 }
 
 
-shinyModuleConfiguration <- function(id, input) {
-  ns <- NS(id)
-  configuration <- list()
-
-  print(ns('posi_lon'))
-  configuration["posi_lon"] <- input[[ns('posi_lon')]]
-
-  print(ns('posi_lat'))
-  configuration["posi_lat"] <- input[[ns('posi_lat')]]
-
-  configuration
-}
-
-
-shinyModule <- function(input, output, session, data, posi_lon=NULL, posi_lat=NULL) {
-  dataObj <- reactive({ data })
+shinyModule <- function(input, output, session, data) {
+  # dataObj <- reactive({ data })
   current <- reactiveVal(data)
-  
+  userCorrds <- reactiveValues(posi_lon = 0, posi_lat = 0)
+  observeEvent(input$GoButton, {
+    userCorrds$posi_lon <- input$posi_lon
+    userCorrds$posi_lat <- input$posi_lat
+  })
   timestamp_range <- range(timestamps(data))
   timestamp_unit30 <- units(difftime(timestamp_range[1],seq(timestamp_range[1],timestamp_range[2],len=30)[2]))
   timestamp_labs <- round(seq(timestamp_range[1],timestamp_range[2],len=30),unit=timestamp_unit30)
   dist_range <- reactive({
-    range(distVincentyEllipsoid(coordinates(data),c(input$posi_lon,input$posi_lat)))
+    range(distVincentyEllipsoid(coordinates(data),c(userCorrds$posi_lon,userCorrds$posi_lat)))
   })
   
   data.split <- move::split(data)
@@ -57,26 +54,31 @@ shinyModule <- function(input, output, session, data, posi_lon=NULL, posi_lat=NU
   
   dist_to_loc <- reactive({
     foreach(datai = data.split) %do% {
-      disti <- distVincentyEllipsoid(coordinates(datai),c(input$posi_lon,input$posi_lat))
+      disti <- distVincentyEllipsoid(coordinates(datai),c(userCorrds$posi_lon,userCorrds$posi_lat))
       timei <- timestamps(datai)
       data.frame(disti,timei)
     }
   })
   
-  csv_out <- foreach(datai = data.split, .combine = rbind) %do% {
-    idi <- rep(namesIndiv(datai),n.locs(datai))
-    cooi <- coordinates(datai)
-    timei <- timestamps(datai)
-    disti <- distVincentyEllipsoid(coordinates(datai),c(posi_lon,posi_lat))
-    data.frame("individual_local_identifier"=idi,"timestamp"=timei,"location_long"=cooi[,1],"location_lat"=cooi[,2],"distance_to_location (m)"=disti)
-  }
-  write.csv(csv_out,file=paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"distance_table.csv"),row.names=FALSE) #this is the output only for the initial setting
-  
+ #  observe({
+  # csv_out <- # reactive({
+  #   foreach(datai = data.split, .combine = rbind) %do% {
+  #   idi <- rep(namesIndiv(datai),n.locs(datai))
+  #   cooi <- coordinates(datai)
+  #   timei <- timestamps(datai)
+  #   disti <- distVincentyEllipsoid(coordinates(datai),c(userCorrds$posi_lon,userCorrds$posi_lat))
+  #   data.frame("individual_local_identifier"=idi,"timestamp"=timei,"location_long"=cooi[,1],"location_lat"=cooi[,2],"distance_to_location (m)"=disti)
+  #   }
+ # # })
+ # 
+ #    write.csv(csv_out,file=paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"),"distance_table.csv"),row.names=FALSE) #this is the output only for the initial setting
+ #  })
+
   
 #output
-  output$value1 <- renderText({ input$posi_lon})
-  output$value2 <- renderText({ input$posi_lat})
-  
+  # output$value1 <- renderText({ input$posi_lon})
+  # output$value2 <- renderText({ input$posi_lat})
+
   output$timeline <- renderPlot({
     par(mar=c(12,4,4,2)+0.1,lab=c(10,6,20))
     plot(timestamp_range,dist_range(),type="l",xlim=timestamp_range,ylim=dist_range(),xlab="",ylab="distance to reference location (m)",col="white",axes=FALSE)
@@ -90,8 +92,16 @@ shinyModule <- function(input, output, session, data, posi_lon=NULL, posi_lat=NU
       lines(dist_to_loc()[[i]]$timei,dist_to_loc()[[i]]$disti,col=cols[i],lwd=2)
       legend("topright",legend,namen,fill=cols)
     }
+    csv_out <- foreach(datai = data.split, .combine = rbind) %do% {
+        idi <- rep(namesIndiv(datai),n.locs(datai))
+        cooi <- coordinates(datai)
+        timei <- timestamps(datai)
+        disti <- distVincentyEllipsoid(coordinates(datai),c(userCorrds$posi_lon,userCorrds$posi_lat))
+        data.frame("individual_local_identifier"=idi,"timestamp"=timei,"location_long"=cooi[,1],"location_lat"=cooi[,2],"distance_to_location (m)"=disti)
+      }
+    write.csv(csv_out,file=appArtifactPath("_distance_table.csv"),row.names=FALSE)
   })
-  
+
   return(reactive({ current() })) 
 }
 
